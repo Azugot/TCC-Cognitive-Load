200bb3055e68c9512147eafe06ee5faa3be33f06
Azugot <augusto.oliveira.1388188@sga.pucminas.br>
2025-10-24 12:15:13 -0300

    comment update

--- app/config.py
+++ app/config.py
@@ -21,8 +21,8 @@ VERTEX_SERVICE_ACCOUNT = {
     "project_id": os.getenv("GCP_PROJECT_ID", ""),
     "private_key_id": os.getenv("GCP_PRIVATE_KEY_ID", ""),
     "private_key": os.getenv("GCP_PRIVATE_KEY", "").replace("\\n", "\n"),
-    "client_email": os.getenv("GCP_CLIENT_EMAIL", ""),    # ok hard-coded
-    "client_id": os.getenv("GCP_CLIENT_ID", ""),          # ok hard-coded
+    "client_email": os.getenv("GCP_CLIENT_EMAIL", ""), 
+    "client_id": os.getenv("GCP_CLIENT_ID", ""),
     "auth_uri": "https://accounts.google.com/o/oauth2/auth",
     "token_uri": "https://oauth2.googleapis.com/token",
     "auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",
